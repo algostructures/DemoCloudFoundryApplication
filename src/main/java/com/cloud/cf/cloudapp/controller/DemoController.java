5aaf2ea7ac43c3933d4bb018f4bf89fb4dd07f54
saurabh <saurabh945644@gmail.com>
2018-10-02 00:03:29 +0530

    First Commit

---
+++ src/main/java/com/cloud/cf/cloudapp/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.cloud.cf.cloudapp.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cf")
+public class DemoController {
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello From Saurabh";
+    }
+}
